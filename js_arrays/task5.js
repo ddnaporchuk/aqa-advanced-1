af807d00a5bd783d436f71db43003be26bd1105a
Den <denisnpqa@gmail.com>
2023-09-18 17:35:18 +0300

    Added homework#5 Arrays

---
+++ js_arrays/task5.js
@@ -0,0 +1,6 @@
+const firstArray = [1, 3, 5, 7, 9];
+const secondArray = [1, 2, 3, 4, 5];
+
+const combinedArray = firstArray.concat(secondArray);
+
+console.log("Об'єднання двох масивів в один:", combinedArray); // Об'єднання двох масивів в один: [1,3,5,7,9,1,2,3,4,5]
